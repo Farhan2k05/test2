7d7951b8934837e141a15829c3b244da04e21844
Farhan Islam <islamfarhan2k5@gmail.com>
2024-03-13 07:55:12 +0000

    made conflicting changes

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Farhan!");
+        System.out.println("Hello from new branch,Farhan!");
     }
 }
